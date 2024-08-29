9647db6ac2c684a79200a7d85912f0de95aae8ab
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-08-29 14:13:52 +0200

    add: added shadow to btn

--- src/components/AppCarousel.vue
+++ src/components/AppCarousel.vue
@@ -65,6 +65,8 @@ button {
   width: 40px;
   height: 40px;
   aspect-ratio: 1;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+
 }
 .img-fluid {
   width: 100%;
@@ -74,5 +76,6 @@ button {
 .disabled{
     background: lightgray;
 }
+
 
 </style>
